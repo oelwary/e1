8e26486ff685a0acb74b113f7c8fde22d6e169ae
Omar Elwary <sui.meliorationem@gmail.com>
2018-02-14 20:32:12 +0200

    Added search

--- index.js
+++ index.js
@@ -2,22 +2,39 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 const request = require('request');
+const url = require('url');
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', function(req, res, next) {
-    baseSearch(res)
+    baseSearch(req, res)
   })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
-function baseSearch(res) {
-    request.get({ url: "https://offersvc.expedia.com/offers/v2/getOffers?scenario=deal-finder&page=foo&uid=foo&productType=Hotel" },
+function baseSearch(req, res) {
+    var url_parts = url.parse(req.url, true);
+    var query = url_parts.query;
+    var queryString = '';
+    for (key in query) {
+        if (query[key]) {
+            queryString = queryString + '&' + key + '=' + encodeURIComponent(query[key]);
+        }
+    }
+    console.log("Query: " + queryString);
+
+    request.get({ url: "https://offersvc.expedia.com/offers/v2/getOffers?scenario=deal-finder&page=foo&uid=foo&productType=Hotel" + queryString},
                 function(error, response, body) {
                   if (!error && response.statusCode == 200) {
                        var data = JSON.parse(body);
-                       res.render('pages/index', data);
+                       if (data["offers"] != undefined && data["offers"]["Hotel"] != undefined && data["offers"]["Hotel"].length > 0 ) {
+                        console.log("Found " + data["offers"]["Hotel"].length + " offers!");
+                        res.render('pages/index', data);
+                       }
+                       else {
+                        res.render('pages/not_found', data);
+                       }
                      }
                  });
 }
